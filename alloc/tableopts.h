07d309ff2bae1e51afc6ea07a9fb0d48473ec3e7
Julian W <julian.waciewski@rwth-aachen.de>
2025-01-20 11:17:31 +0100

    Add header entries

--- alloc/tableopts.h
+++ alloc/tableopts.h
@@ -1,8 +1,8 @@
 #ifndef ALLOC_TABLEOPTS_H
 #define ALLOC_TABLEOPTS_H
 
-#include "alloc/types.h"
 #include <stddef.h>
+#include <stdint.h>
 
 void init_table ();
 
@@ -13,5 +13,9 @@ int remove_map_entry (const uint8_t *start);
 int memset_zero (uint8_t *start);
 
 int move_mem (uint8_t *old, uint8_t *new, size_t size);
+
+bool check_heap_integrity ();
+
+size_t get_heap_used_space ();
 
 #endif
