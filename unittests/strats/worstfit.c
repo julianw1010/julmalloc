143ff2f62a4d3c3ac2ebe5f215249fe6c41d71ec
Julian W <julian.waciewski@rwth-aachen.de>
2025-07-04 16:41:31 +0200

    Updated worst_fit test

--- unittests/strats/worstfit.c
+++ unittests/strats/worstfit.c
@@ -19,108 +19,34 @@ size_t sum_aligned(size_t num_gaps) {
     return sum;
 }
 
-// Create a grid of gap sizes ascending by 1, separated by allocated memory of
-// size 1
-// Like this:
-// 1 * 2 * 3 * ...
-// where the number denotes the gap size and * denotes allocated storage of size
-// 1
-// If first_fit works correctly, the first fitting gap should be allocated
-int grid_test() {
+int two_test() {
 
-    pr_info("Alloc");
-    uint8_t *anchor = malloc(1);
-    anchor += ALIGNMENT + sizeof(struct seg_tail_s);
-    ASSERT(is_aligned(anchor));
+    uint8_t *expand = malloc(STORAGE_SIZE_TESTING);
+    uint8_t *end = malloc(1);
+    free(expand);
 
-    for (size_t aligned_storage_s = PAGE_SIZE; aligned_storage_s <= STORAGE_SIZE_TESTING ; aligned_storage_s+=PAGE_SIZE) {
+    for (size_t i = 0; i < STORAGE_SIZE_TESTING/4;i++) {
 
-    size_t num_gaps = 1;
-
-    // Measure maximum number of gaps. The last gap has to be strictly larger
-    // than the previous ones
-    while (sum_aligned(num_gaps+1) + (num_gaps+1)* ALIGNMENT +
-               (2 * (num_gaps+1)+1) *
-                   (sizeof(struct seg_head_s) + sizeof(struct seg_tail_s)) <=
-           aligned_storage_s) {
-        num_gaps++;
-    }
-
-    num_gaps--;
-
-    uint8_t *segments[num_gaps-1];
-
-    uint8_t *barriers[num_gaps-1];
-
-    // Add "barriers between gaps"
-    for (size_t i = 0; i < num_gaps - 1; i++) {
-        pr_info("True");
-
-        pr_info("Alloc");
-        segments[i] = malloc(i + 1);
-
-        pr_info("Alloc");
+        uint8_t *buffer = malloc(i);
         uint8_t *barrier = malloc(1);
-        barriers[i] = barrier;
-        ASSERT((uintptr_t)segments[i] % ALIGNMENT == 0)
-        ASSERT((uintptr_t)barrier % ALIGNMENT == 0)
-        ASSERT(is_aligned(segments[i]) && is_aligned(barrier));
 
-        pr_info("" FMT_UINTPTR, barrier - segments[i]);
-        ASSERT(barrier == segments[i] + round_up(i + 1, ALIGNMENT) +
-                              sizeof(struct seg_head_s) +
-                              sizeof(struct seg_tail_s))
-        ASSERT(segments[i] ==
-               (uint8_t *)anchor + (sum_aligned(i) + i * ALIGNMENT +
-                                    (2 * i) * (sizeof(struct seg_head_s) +
-                                               sizeof(struct seg_tail_s)) +
-                                    sizeof(struct seg_head_s)));
-        ASSERT(barrier ==
-               (uint8_t *)anchor + (sum_aligned(i + 1) + i * ALIGNMENT +
-                                    (2 * i + 1) * (sizeof(struct seg_head_s) +
-                                                   sizeof(struct seg_tail_s)) +
-                                    sizeof(struct seg_head_s)));
+        free(buffer);
+
+        uint8_t *test = malloc(1);
+
+        ASSERT(test == barrier + 1*ALIGNMENT + sizeof(struct seg_tail_s) + sizeof(struct seg_head_s));
+
+        free(test);
+
+        free(barrier);
     }
-
-    for (size_t i = 0; i < num_gaps-1; i++) {
-        pr_info("Freeing address %p", segments[i]);
-        free(segments[i]);
-    }
-
-
-    set_alloc_function(WORST_FIT);
-
-    // Allocate num_gaps many elements of ascending size and check if they
-    // land in the right grid
-    for (size_t i = 0; i < num_gaps/2; i++) {
-
-        pr_info("Alloc");
-        uint8_t *addr = malloc(i + 1);
-        pr_info("Num gaps: %zu", num_gaps);
-        ASSERT(is_aligned(addr));
-        pr_info("i %zu aligned_storage %zu", i, aligned_storage_s);
-
-        ASSERT(addr == (uint8_t *)anchor + sizeof(struct seg_head_s) + 
-                            sum_aligned(num_gaps-1) +
-                             (num_gaps-1) * ALIGNMENT +
-                             (2 * (num_gaps-1)) *
-                                 (sizeof(struct seg_head_s) +
-                                  sizeof(struct seg_tail_s)));
-
-        free(addr);
-    }
-
-    for (size_t i = 0; i < num_gaps-1; i++) {
-        free(barriers[i]);
-    }
-
-
-}
 
     return EXIT_SUCCESS;
+
 }
+
 
 int main() {
     set_alloc_function(WORST_FIT);
-    return (grid_test());
+    return (two_test());
 }
